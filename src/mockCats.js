f6315851de533c05f21cfe6bf32c11d8dded56a6
Sarah Proctor <sproctor950@gmail.com>
2024-03-29 13:43:08 -0700

    :seedling: adds mock cats

---
+++ src/mockCats.js
@@ -0,0 +1,21 @@
+let cats = [
+  {
+    id: 1,
+    name: "Garfield",
+    age: 12,
+    enjoys: "lasagna and kicking Odie"
+  },
+  {
+    id: 2,
+    name: "Oliver",
+    age: 1,
+    enjoys: "being super posh"
+  },
+  {
+    id: 3,
+    name: "Sylvester",
+    age: 6,
+    enjoys: "sufferin succotash"
+  }
+]
+export default cats
