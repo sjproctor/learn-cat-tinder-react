152fd023d6f83694a94ba46e8be7c2cfdd66e04e
Sarah Proctor <sproctor950@gmail.com>
2024-03-29 16:02:39 -0700

    :seedling: adds additional mock cats

--- src/mockCats.js
+++ src/mockCats.js
@@ -3,19 +3,40 @@ let cats = [
     id: 1,
     name: "Garfield",
     age: 12,
-    enjoys: "lasagna and kicking Odie"
+    enjoys: "lasagna and kicking Odie",
+    image: "https://i.ebayimg.com/images/g/m5IAAOSw03lY7miz/s-l1600.jpg"
   },
   {
     id: 2,
     name: "Oliver",
     age: 1,
-    enjoys: "being super posh"
+    enjoys: "being super posh",
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a6e1398e-141f-4f77-8212-12c7f8c40dff/dg1ik98-ff3c3b0a-09c0-4160-9d05-9af6d9210c6b.jpg/v1/fit/w_600,h_600,q_70,strp/31_cats_of_july__oliver_by_jrg2004_dg1ik98-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjAwIiwicGF0aCI6IlwvZlwvYTZlMTM5OGUtMTQxZi00Zjc3LTgyMTItMTJjN2Y4YzQwZGZmXC9kZzFpazk4LWZmM2MzYjBhLTA5YzAtNDE2MC05ZDA1LTlhZjZkOTIxMGM2Yi5qcGciLCJ3aWR0aCI6Ijw9NjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.u1bYfk_hSHCfwo-4lB3gtERIbIvcaCXh8vHzhSHg6p0"
   },
   {
     id: 3,
     name: "Sylvester",
     age: 6,
-    enjoys: "sufferin succotash"
+    enjoys: "sufferin succotash",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/8/82/Sylvester_the_Cat.svg/1024px-Sylvester_the_Cat.svg.png"
+  },
+  {
+    id: 4,
+    name: "Felix",
+    age: 103,
+    enjoys: "being the very first Macy's Thanksgiving parade balloon",
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c974406c-cdbc-4a3c-9e4a-6ef95c9e107d/dciv37m-4cf33216-a0d5-4c55-98f0-1c7b1137d0ae.jpg/v1/fit/w_828,h_1072,q_70,strp/felix_el_gato_by_edwardcrow_dciv37m-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTMyNiIsInBhdGgiOiJcL2ZcL2M5NzQ0MDZjLWNkYmMtNGEzYy05ZTRhLTZlZjk1YzllMTA3ZFwvZGNpdjM3bS00Y2YzMzIxNi1hMGQ1LTRjNTUtOThmMC0xYzdiMTEzN2QwYWUuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.L-mtRfhgW-8baz60mp3I44KiKOs3Zu2hUzepCYFkIlI"
+  },
+  {
+    id: 5,
+    name: "Cheshire Cat",
+    age: 13,
+    enjoys: "just smiling and smiling",
+    image:
+      "https://ih1.redbubble.net/image.2844651963.3788/flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
   }
 ]
 export default cats
