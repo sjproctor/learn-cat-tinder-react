fe728566b4b6707510f055eed9bb41591893c3b0
Sarah Proctor <sproctor950@gmail.com>
2024-04-01 12:11:04 -0700

    :sparkles: defines an id param for the show route and passes in the cats from state

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cat-index" element={<CatIndex cats={cats} />} />
-        <Route path="/cat-show" element={<CatShow />} />
+        <Route path="/cat-show/:id" element={<CatShow cats={cats} />} />
         <Route path="/cat-new" element={<CatNew />} />
         <Route path="/cat-edit" element={<CatEdit />} />
         <Route path="*" element={<NotFound />} />
