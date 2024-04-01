54dccc6db488dfc7398eae4c6c98ad49b4c3c51c
Sarah Proctor <sproctor950@gmail.com>
2024-04-01 12:10:01 -0700

    :sparkles: adds cat show page, styling, and passing tests

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -1,7 +1,23 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
-const CatShow = () => {
-  return <>show</>
+const CatShow = ({ cats }) => {
+  const { id } = useParams()
+  const cat = cats.find((item) => item.id === +id)
+  return (
+    <div className="page-body">
+      <h3 className="handrawn-font cat-name">Meet {cat.name}</h3>
+      <p>Age {cat.age}</p>
+      <p>
+        {cat.name} enjoys {cat.enjoys}.
+      </p>
+      <img
+        src={cat.image}
+        alt={`profile of ${cat.name}`}
+        className="cat-image"
+      />
+    </div>
+  )
 }
 
 export default CatShow
---
+++ src/pages/CatShow.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react"
+import CatShow from "../pages/CatShow"
+import { MemoryRouter, Routes, Route } from "react-router-dom"
+import mockCats from "../mockCats.js"
+
+describe("<CatShow />", () => {
+  it("renders one cat", () => {
+    render(
+      <MemoryRouter initialEntries={["/cat-show/1"]}>
+        <Routes>
+          <Route path="/cat-show/:id" element={<CatShow cats={mockCats} />} />
+        </Routes>
+      </MemoryRouter>
+    )
+    const cat = mockCats.find((item) => item.id === 1)
+    const catName = screen.getByText(`Meet ${cat.name}`)
+    expect(catName).toBeInTheDocument()
+    const catAge = screen.getByText(`Age ${cat.age}`)
+    expect(catAge).toBeInTheDocument()
+    const catEnjoys = screen.getByText(`${cat.name} enjoys ${cat.enjoys}.`)
+    expect(catEnjoys).toBeInTheDocument()
+    const catImage = screen.getByAltText(`profile of ${cat.name}`)
+    expect(catImage).toBeInTheDocument()
+    expect(catImage).toHaveAttribute("src", cat.image)
+  })
+})
