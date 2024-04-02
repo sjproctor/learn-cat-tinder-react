d936d583e1e28c15eb1a5070e646e341673b0d72
Sarah Proctor <sproctor950@gmail.com>
2024-04-02 16:04:24 -0700

    :sparkles: added fetch requests and removed mock data to connect local Rails API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
 import Footer from "./components/Footer"
@@ -9,20 +9,80 @@ import CatNew from "./pages/CatNew"
 import CatShow from "./pages/CatShow"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
-import mockCats from "./mockCats"
 
 const App = () => {
-  const [cats, setCats] = useState(mockCats)
-  const createNewCat = (newCat) => {
-    console.log(newCat)
+  const [cats, setCats] = useState([])
+  console.log(cats)
+  useEffect(() => {
+    getCats()
+  }, [])
+
+  const getCats = async () => {
+    try {
+      const getResponse = await fetch("http://localhost:3000/cats")
+      if (!getResponse.ok) {
+        throw new Error("Error on cat get request")
+      }
+      const getResult = await getResponse.json()
+      setCats(getResult)
+    } catch (error) {
+      console.error("Error fetching data:", error.message)
+    }
   }
-  const editCat = (cat, id) => {
-    console.log("edit:", cat)
-    console.log("edit:", id)
+
+  const createNewCat = async (newCat) => {
+    try {
+      const postResponse = await fetch("http://localhost:3000/cats", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newCat)
+      })
+      if (!postResponse.ok) {
+        throw new Error("Error on cat post request")
+      }
+      await postResponse.json()
+      getCats()
+    } catch (error) {
+      console.error("Error fetching data:", error.message)
+    }
   }
-  const deleteCat = (id) => {
-    console.log("delete:", id)
+
+  const editCat = async (cat, id) => {
+    try {
+      const postResponse = await fetch(`http://localhost:3000/cats/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(cat)
+      })
+      if (!postResponse.ok) {
+        throw new Error("Error on cat post request")
+      }
+      await postResponse.json()
+      getCats()
+    } catch (error) {
+      console.error("Error fetching data:", error.message)
+    }
   }
+
+  const deleteCat = async (id) => {
+    try {
+      // Send DELETE request to delete the specified cat
+      const response = await fetch(`http://localhost:3000/cats/${id}`, {
+        method: "DELETE"
+      })
+      if (!response.ok) {
+        throw new Error("Error deleting cat")
+      }
+      getCats()
+    } catch (error) {
+      console.error("Error deleting cat:", error.message)
+    }
+  }
+
   return (
     <>
       <Header />
